503811829b85788b028c85dc404cc6a6ea274ea5
Teun Mooij <tmooij@infinitaslearning.com>
2024-06-16 19:06:44 +0200

    fix type exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export type { Component, ComponentTypeOf, DependenciesOf, IsComponent } from './component';
 export type { Definition, Registration } from './definition';
+export type { DependsOnOption } from './dependencies';
 export type { SystemOf } from './system';
-export type { DependsOnOption, Systemic, SystemicBuild } from './systemic';
+export type { Systemic, SystemicBuild } from './systemic';
 export type { EmptyObject } from './util';
